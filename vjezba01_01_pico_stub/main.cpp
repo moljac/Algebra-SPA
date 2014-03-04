82c1a3f9eb076f401505a4d7f094cdcaef33ce42
moljac <mcvjetko@holisticware.net>
2014-03-04 15:39:17 +0100

    Vjezba 01 2 samples

---
+++ vjezba01_01_pico_stub/main.cpp
@@ -0,0 +1,10 @@
+// no includes
+
+int main
+	(
+	// command line arguments
+	//int argc, char *argv[]
+	)
+{
+	return 0;
+}
---
+++ vjezba01_02_mini_stub/main.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+int main
+	(
+	// command line arguments
+	//int argc, char *argv[]
+	)
+{
+	std::cout << "Hello world" << std::endl;
+
+	std::cin.get();
+	system("pause");
+
+	return 0;
+}
